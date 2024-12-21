fde83b2d6a976aa43bd3ac40aa2f0f0833225881
David Greaves <david@dgreaves.com>
2024-12-21 11:33:39 +0000

    Tests: Use a fixture for strips and pytest-cov support
    Signed-off-by: David Greaves <david@dgreaves.com>

--- library/tests/test_setup.py
+++ library/tests/test_setup.py
@@ -1,5 +1,11 @@
 import pytest
 
+@pytest.fixture()
+def strip(_rpi_ws281x):
+    from rpi_ws281x import PixelStrip
+    strip = PixelStrip(10, 20)
+    strip.begin()
+    yield strip
 
 def test_setup(_rpi_ws281x):
     from rpi_ws281x import PixelStrip
@@ -15,19 +21,14 @@ def test_setup_init_fail(_rpi_ws281x):
         strip.begin()
 
 
-def test_num_pixels(_rpi_ws281x):
-    from rpi_ws281x import PixelStrip
-    strip = PixelStrip(10, 20)
-    strip.begin()
+def test_num_pixels(strip):
     assert len(strip[:]) == 10
     assert len(strip) == 10
     assert strip.numPixels() == 10
 
 
-def test_set_pixel(_rpi_ws281x):
-    from rpi_ws281x import PixelStrip, RGBW
-    strip = PixelStrip(10, 20)
-    strip.begin()
+def test_set_pixel(strip):
+    from rpi_ws281x import RGBW
     strip[0] = RGBW(255, 0, 0)
     assert strip[0] == strip.getPixelColor(0)
     assert strip[0] == RGBW(255, 0, 0)
@@ -36,17 +37,13 @@ def test_set_pixel(_rpi_ws281x):
     assert strip.getPixelColorRGBW(0).r == 255
 
 
-def test_set_multiple(_rpi_ws281x):
-    from rpi_ws281x import PixelStrip, RGBW
-    strip = PixelStrip(10, 20)
-    strip.begin()
+def test_set_multiple(strip):
+    from rpi_ws281x import RGBW
     strip[:] = RGBW(255, 0, 0)
     assert strip[:] == [RGBW(255, 0, 0)] * 10
 
 
-def test_set_odd(_rpi_ws281x):
-    from rpi_ws281x import PixelStrip, RGBW
-    strip = PixelStrip(10, 20)
-    strip.begin()
+def test_set_odd(strip):
+    from rpi_ws281x import RGBW
     strip[::2] = RGBW(255, 0, 0)
     assert strip[:] == [RGBW(255, 0, 0), RGBW(0, 0, 0)] * 5
